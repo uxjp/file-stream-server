483f1f9e8167a93717abc6fb5ab08f829f6cb95f
JoaoPedro <joaopedro1172@gmail.com>
2023-04-12 13:29:13 -0300

    save .txt sent as octet-stream

--- http/server.js
+++ http/server.js
@@ -1,25 +1,29 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'POST' && req.url === '/upload') {
+    if (req.method === 'POST' && req.url === '/txt') {
       let data = '';
+
       req.on('data', (chunk) => {
         data += chunk;
-      });
+      })
+
       req.on('end', () => {
-        const buffer = Buffer.from(data, 'binary');
-        fs.writeFile('file.bin', buffer, (err) => {
+
+        fs.writeFile(path.join(__dirname, 'file1.txt'), data, (err) => {
           if (err) {
-            console.error(err);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Internal Server Error');
+            console.error(err)
+            res.statusCode = 500
+            res.end('Error writing to file')
           } else {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('File saved');
+            res.statusCode = 200
+            res.end('File saved successfully')
           }
         });
-      });
+      })
+
     } else {
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('Not Found');
@@ -27,6 +31,6 @@ const server = http.createServer((req, res) => {
   });
   
   server.listen(3000, () => {
-    console.log('Server started on port 3000 >>');
+    console.log('Server started on port 3000');
   });
   
